60592c3f0dcdd5ee279d9a3dc1a85fdeb5313eab
lcdwiki <39901169+lcdwiki@users.noreply.github.com>
2018-06-21 16:11:14 +0800

    Add files via upload

---
+++ Example/Example_01_clear_screen/clear_Screen/clear_Screen.ino
@@ -0,0 +1,67 @@
+// IMPORTANT: LCDWIKI_SPI LIBRARY MUST BE SPECIFICALLY
+// CONFIGURED FOR EITHER THE TFT SHIELD OR THE BREAKOUT BOARD.
+
+//This program is a demo of clearing screen to display black,white,red,green,blue.
+
+//Set the pins to the correct ones for your development shield or breakout board.
+//when using the BREAKOUT BOARD only and using these software spi lines to the LCD,
+//there is no MISO pin and You can use any free pin to define the pins,for example
+//pin usage as follow:
+//             CS  CD  RST  MOSI  MISO  CLK    
+//Arduino Uno  A5  A3  A4   A2    NONE  A1
+//Arduino Mega A5  A3  A4   A2    NONE  A1
+
+//when using the BREAKOUT BOARD only and using these hardware spi lines to the LCD,
+//there is no MISO pin 
+//the MOSI pin and CLK pin is defined by the system and can't be modified.
+//other pins can be defined by youself,for example
+//pin usage as follow:
+//             CS  CD  RST  MOSI  MISO  CLK    
+//Arduino Uno  10  9   8    11    NONE  13
+//Arduino Mega 10  9   8    51    NONE  52
+
+//Remember to set the pins to suit your display module!
+
+#include <LCDWIKI_GUI.h> //Core graphics library
+#include <LCDWIKI_SPI.h> //Hardware-specific library
+
+//the definiens of software spi mode as follow:
+//if the IC model is known or the modules is unreadable,you can use this constructed function
+//LCDWIKI_SPI mylcd(ST7735S,A5,A3,-1,A2,A4,A1);//software spi,model,cs,cd,miso,mosi,reset,clk
+
+//if the IC model is not known and the modules is readable,you can use this constructed function
+//LCDWIKI_SPI mylcd(160,128,A5,A3,-1,A2,A4,A1);//software spi,model,cs,cd,miso,mosi,reset,clk
+
+//the definiens of hardware spi mode as follow:
+//if the IC model is known or the modules is unreadable,you can use this constructed function
+//LCDWIKI_SPI mylcd(ST7735S,10,9,8); //hardware spi,cs,cd,reset
+
+//if the IC model is not known and the modules is readable,you can use this constructed function
+//LCDWIKI_SPI mylcd(160,128,10,9,8); //hardware spi,cs,cd,reset
+
+void setup() 
+{
+    mylcd.Init_LCD();
+    mylcd.Fill_Screen(0x0000);
+    mylcd.Fill_Screen(0xFFFF);
+}
+
+void loop() 
+{    
+    mylcd.Fill_Screen(0,0,0);
+    mylcd.Fill_Screen(255,255,255);
+    mylcd.Fill_Screen(255,0,0); 
+    mylcd.Fill_Screen(0,255,0);
+    mylcd.Fill_Screen(0,0,255);
+    delay(3000);
+    mylcd.Fill_Screen(0,0,0);
+    delay(1000);
+    mylcd.Fill_Screen(255,255,255);
+    delay(1000);
+    mylcd.Fill_Screen(0xF800);
+    delay(1000);
+    mylcd.Fill_Screen(0x07E0);
+   delay(1000);
+   mylcd.Fill_Screen(0x001F);
+   delay(3000);
+}
---
+++ LCDWIKI_SPI.h
@@ -0,0 +1,96 @@
+// Lcdwiki GUI library with init code from Rossum
+// MIT license
+
+#ifndef _LCDWIKI_SPI_H_
+#define _LCDWIKI_SPI_H_
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#ifdef __AVR__
+#include <avr/pgmspace.h>
+#elif defined(ESP8266)
+ #include <pgmspace.h>
+#else
+#define pgm_read_byte(addr) (*(const unsigned char *)(addr))
+#define pgm_read_word(addr) (*(const unsigned short *)(addr))
+#endif
+#include "LCDWIKI_GUI.h"
+
+// LCD controller chip identifiers
+#define ID_932X    0
+#define ID_7575    1
+#define ID_9341    2
+#define ID_HX8357D    3
+#define ID_4535    4
+#define ID_9486    5
+#define ID_7735    6
+#define ID_UNKNOWN 0xFF
+
+//LCD controller chip mode identifiers
+#define ILI9325 0
+#define ILI9328 1
+#define ILI9341 2
+#define HX8357D 3
+#define HX8347G 4
+#define HX8347I 5
+#define ILI9486 6
+#define ST7735S 7
+
+typedef struct _lcd_info
+{
+	uint16_t lcd_id;
+	int16_t lcd_wid;
+	int16_t lcd_heg;
+}lcd_info;
+
+class LCDWIKI_SPI:public LCDWIKI_GUI
+{
+	public:
+	LCDWIKI_SPI(uint16_t model,int8_t cs, int8_t cd, int8_t miso, int8_t mosi, int8_t reset, int8_t clk);
+	LCDWIKI_SPI(uint16_t model,int8_t cs, int8_t cd, int8_t reset);
+	LCDWIKI_SPI(int16_t wid,int16_t heg,int8_t cs, int8_t cd, int8_t miso, int8_t mosi, int8_t reset, int8_t clk);
+	LCDWIKI_SPI(int16_t wid,int16_t heg,int8_t cs, int8_t cd, int8_t reset);
+	void Init_LCD(void);
+	void reset(void);
+	void start(uint16_t ID);
+	void Draw_Pixe(int16_t x, int16_t y, uint16_t color);
+	void Spi_Write(uint8_t data);
+	uint8_t Spi_Read(void);
+	void Write_Cmd(uint16_t cmd);
+	void Write_Data(uint16_t data);
+	void Write_Cmd_Data(uint16_t cmd, uint16_t data);
+	void init_table8(const void *table, int16_t size);
+	void init_table16(const void *table, int16_t size);
+	void Push_Command(uint16_t cmd, uint8_t *block, int8_t N);
+	uint16_t Color_To_565(uint8_t r, uint8_t g, uint8_t b);
+	uint16_t Read_ID(void);
+	void Fill_Rect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
+	void Set_Rotation(uint8_t r); 
+	uint8_t Get_Rotation(void) const;
+	void Invert_Display(boolean i);
+	uint16_t Read_Reg(uint16_t reg, int8_t index);
+	int16_t Read_GRAM(int16_t x, int16_t y, uint16_t *block, int16_t w, int16_t h);
+	void Set_Addr_Window(int16_t x1, int16_t y1, int16_t x2, int16_t y2);
+	void Push_Any_Color(uint16_t * block, int16_t n, bool first, uint8_t flags);
+	void Push_Any_Color(uint8_t * block, int16_t n, bool first, uint8_t flags);
+    void Vert_Scroll(int16_t top, int16_t scrollines, int16_t offset);
+	int16_t Get_Height(void) const;
+  	int16_t Get_Width(void) const;
+	void Set_LR(void);
+
+	protected:
+    uint16_t WIDTH,HEIGHT,width, height, rotation,lcd_driver,lcd_model;
+	boolean hw_spi;
+	private:
+	uint16_t XC,YC,CC,RC,SC1,SC2,MD,VL,R24BIT;
+ 
+		 volatile int8_t *spicsPort, *spicdPort, *spimisoPort , *spimosiPort, *spiclkPort;
+			      int8_t  spicsPinSet, spicdPinSet  ,spimisoPinSet , spimosiPinSet , spiclkPinSet,
+						   spicsPinUnset, spicdPinUnset, spimisoPinUnset,  spimosiPinUnset,spiclkPinUnset,
+						   _reset;
+};
+#endif
